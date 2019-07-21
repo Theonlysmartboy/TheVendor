d45bfecabc2525a38e7f44387de8f6c35544bdea
TheOnlySmartBoy <o2jose43@gmail.com>
2019-07-21 16:30:17 +0300

    Added gridlayout look to drinks

--- app/src/main/java/com/otemainc/thevendor/EcohimActivity.java
+++ app/src/main/java/com/otemainc/thevendor/EcohimActivity.java
@@ -10,6 +10,7 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
@@ -65,7 +66,7 @@ public class EcohimActivity extends AppCompatActivity implements NavigationView.
         navigationView.setNavigationItemSelectedListener(this);
         recyclerView = findViewById(R.id.recyclerView);
         recyclerView.setHasFixedSize(true);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setLayoutManager(new GridLayoutManager(this, 3));
         item = findViewById(R.id.item);
 
 
